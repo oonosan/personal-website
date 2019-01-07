82d5f68842f88fc23ebc9192b2f7ca96ee323f9b
Karen <31966611+oonosan@users.noreply.github.com>
2019-01-07 02:35:32 -0300

    Add files via upload

--- assets/js/animation.js
+++ assets/js/animation.js
@@ -1,4 +1,13 @@
 $(function() {
+  $('.scroll-down-home').click(function() {
+    $('html, body').animate({
+      scrollTop: $('section.welcome').offset().top
+    }, 'slow');
+    return false;
+  });
+});
+
+$(function() {
   $('.scroll-down').click(function() {
     $('html, body').animate({
       scrollTop: $('section.cv').offset().top
@@ -8,9 +17,18 @@ $(function() {
 });
 
 $(function() {
-  $('.scroll-down-cv').click(function() {
+  $('.scroll-down-portfolio').click(function() {
     $('html, body').animate({
       scrollTop: $('section.portfolio').offset().top
+    }, 'slow');
+    return false;
+  });
+});
+
+$(function() {
+  $('.scroll-down-contact').click(function() {
+    $('html, body').animate({
+      scrollTop: $('footer').offset().top
     }, 'slow');
     return false;
   });
