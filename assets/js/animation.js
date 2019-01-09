0485766648f6549c0d83d06003b7eeac1313ddaa
Karen Ono <onokarent@gmail.com>
2019-01-09 03:32:20 -0300

    doubled code deleted

--- assets/js/animation.js
+++ assets/js/animation.js
@@ -33,39 +33,3 @@ $(function() {
     return false;
   });
 });
-
-$(function() {
-  $('.scroll-down-home').click(function() {
-    $('html, body').animate({
-      scrollTop: $('section.welcome').offset().top
-    }, 'slow');
-    return false;
-  });
-});
-
-$(function() {
-  $('.scroll-down').click(function() {
-    $('html, body').animate({
-      scrollTop: $('section.cv').offset().top
-    }, 'slow');
-    return false;
-  });
-});
-
-$(function() {
-  $('.scroll-down-portfolio').click(function() {
-    $('html, body').animate({
-      scrollTop: $('section.portfolio').offset().top
-    }, 'slow');
-    return false;
-  });
-});
-
-$(function() {
-  $('.scroll-down-contact').click(function() {
-    $('html, body').animate({
-      scrollTop: $('footer').offset().top
-    }, 'slow');
-    return false;
-  });
-});
